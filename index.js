a628e499c9b4afe5b19d7a5336e6af6a5889efdf
Collexasoft <collexasoft@gmail.com>
2018-08-26 23:16:04 +0530

    made changes to hello world message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!Hello World original !!!');
+  res.send('!Hello World original !!!' + process.env.USER);
 });
 
 var server = app.listen(3000, function () {
